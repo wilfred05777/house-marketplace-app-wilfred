53143fce7b024e1bed3498562cfa7cd990412e9a
Wilfred <wilfred5777@gmail.com>
2022-04-07 14:18:02 +0800

    gist and firestore rules

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,22 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfzHyrI3k7D8FUPTJ6FFjGGVbnPDKJqZ8",
-  authDomain: "house-marketplace-app-52fa4.firebaseapp.com",
-  projectId: "house-marketplace-app-52fa4",
-  storageBucket: "house-marketplace-app-52fa4.appspot.com",
-  messagingSenderId: "664783942850",
-  appId: "1:664783942850:web:6928952f6ebcac69b6b75b",
+  apiKey: "AIzaSyB-tx22LiBqdC6kqAHcP8h6vzYkp15e998",
+  authDomain: "home-marketplace-8a786.firebaseapp.com",
+  projectId: "home-marketplace-8a786",
+  storageBucket: "home-marketplace-8a786.appspot.com",
+  messagingSenderId: "533024292292",
+  appId: "1:533024292292:web:f9340c31e924013267b891",
+  measurementId: "G-C9YYN07BGH",
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDfzHyrI3k7D8FUPTJ6FFjGGVbnPDKJqZ8",
+//   authDomain: "house-marketplace-app-52fa4.firebaseapp.com",
+//   projectId: "house-marketplace-app-52fa4",
+//   storageBucket: "house-marketplace-app-52fa4.appspot.com",
+//   messagingSenderId: "664783942850",
+//   appId: "1:664783942850:web:6928952f6ebcac69b6b75b",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
