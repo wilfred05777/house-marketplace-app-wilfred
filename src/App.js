61c2f9bf087c9049d94435e94c5191eb2a376760
Wilfred <wilfred05777@gmail.com>
2022-05-21 05:55:09 +0800

    94. Explore Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           </Route>
 
           <Route path="/offer" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
