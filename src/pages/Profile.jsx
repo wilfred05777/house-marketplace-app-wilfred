5610eeeabfa8eceb02929c3b5d486d27b4ce259f
Wilfred <wilfred05777@gmail.com>
2022-05-26 05:02:24 +0800

    98. Start Create Listing Page - Profile link 2 CLP

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,6 +15,8 @@ import {
 import { db } from "../firebase.config";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 function Profile() {
   const auth = getAuth();
@@ -109,6 +111,12 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt="arrow right" />
+        </Link>
       </main>
     </div>
   );
