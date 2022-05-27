81b8ae054983c18d251a0a36823e4fba0ca84d88
Wilfred <wilfred05777@gmail.com>
2022-05-28 07:49:34 +0800

    106. Contact Landlord Page - bug

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -30,11 +31,11 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
-
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+          <Route path="/contact/:landlordId" element={<Contact />} />
 
           <Route path="/logout" element={<SignUp />} />
         </Routes>
