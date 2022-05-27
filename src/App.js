37ba32e06ed316c6414d10910398784c0c647bfb
Wilfred <wilfred05777@gmail.com>
2022-05-28 06:54:06 +0800

    104. Fetch Single Listing - not fetching

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -29,6 +30,11 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
 
           <Route path="/logout" element={<SignUp />} />
         </Routes>
